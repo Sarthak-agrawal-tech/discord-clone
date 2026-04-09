af362742ddef2d79a76aa91a6a490a9d0aee61c3
sarthak <sarthak.agrawal.co@gmail.com>
2026-04-09 22:36:46 +0530

    set up the basic folder structure

---
+++ discord/app/(main)/(routes)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return( 
+  <div className="">
+    <p className="text-4xl text-indigo-500 font-bold">
+      This is a protected route.
+    </p>
+  </div>
+    );
+}
