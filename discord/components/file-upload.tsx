bcb3b37079605952f0d49ebe0991a0486ba7b8dd
sarthak <sarthak.agrawal.co@gmail.com>
2026-04-19 21:53:16 +0530

    finished the uploadthing image uploading feature

--- discord/components/file-upload.tsx
+++ discord/components/file-upload.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import { X } from "lucide-react";
 
 import { UploadDropzone } from "@uploadthing/react";
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
@@ -25,6 +26,12 @@ export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
                     alt="Upload"
                     className="rounded-full"
                 />
+                <button onClick={() => onChange("")} 
+                    className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+                  >
+                  <X
+                  className="h-4 w-4"/>
+                </button>
             </div>
         )
     }
