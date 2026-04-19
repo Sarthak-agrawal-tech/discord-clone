1b8fb17a4abf689e6237696a273e33837e326cfd
sarthak <sarthak.agrawal.co@gmail.com>
2026-04-19 22:29:37 +0530

    created: API to create the server and admin member in the database

--- discord/components/modals/initial-models.tsx
+++ discord/components/modals/initial-models.tsx
@@ -1,5 +1,6 @@
 "use client";
 import * as z from "zod";
+import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import {
@@ -23,6 +24,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { FileUpload } from "@/components/file-upload";
+import { useRouter } from "next/navigation";
 
 
 const formSchema = z.object({
@@ -35,6 +37,8 @@ const formSchema = z.object({
 });
 
 export const InitialModel = () => {
+  const router = useRouter();
+
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -46,7 +50,16 @@ export const InitialModel = () => {
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    try{
+      await axios.post("/api/servers", values);
+
+      form.reset();
+      router.refresh();
+      window.location.reload();
+
+    }catch(error){
+      console.log(error);
+    }
   };
 
   return (
