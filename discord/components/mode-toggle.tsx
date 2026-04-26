99657cf34fff11919c7ae6a855936587756748d4
sarthak <sarthak.agrawal.co@gmail.com>
2026-04-26 14:55:45 +0530

    bug fix: fixed the dark mode and light mode toogle button icon not rendering perfectly

--- discord/components/mode-toggle.tsx
+++ discord/components/mode-toggle.tsx
@@ -14,7 +14,7 @@ import {
 } from "@/components/ui/dropdown-menu"
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+  const { setTheme,resolvedTheme } = useTheme()
   const [mounted, setMounted] = React.useState(false)
 
   React.useEffect(() => {
@@ -22,19 +22,18 @@ export function ModeToggle() {
   }, [])
 
   if (!mounted) {
-    return null;
+    return <div className={buttonVariants({ variant: "outline", size: "icon" })} />;
   }
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className={buttonVariants({  variant: "outline", size: "icon" })}
-  
-      >
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+      <DropdownMenuTrigger className={buttonVariants({  variant: "outline", size: "icon" })}>
+        {resolvedTheme === "dark" ? (
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+        ) : (
+          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+        )}
           <span className="sr-only">Toggle theme</span>
-        </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
