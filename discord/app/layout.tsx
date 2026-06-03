c339dea8ca103b1ed877ff680d2d3e599a4b3b2b
sarthak <sarthak.agrawal.co@gmail.com>
2026-06-03 16:55:29 +0530

    updated to the next themes newest update

--- discord/app/layout.tsx
+++ discord/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ThemeProvider } from "@teispace/next-themes";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { cn } from "@/lib/utils";
 
--- discord/components/mode-toggle.tsx
+++ discord/components/mode-toggle.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import { Moon, Sun } from "lucide-react"
-import { useTheme } from "next-themes"
+import { useTheme } from "@teispace/next-themes"
 import { buttonVariants } from "@/components/ui/button"
 
 import { Button } from "@/components/ui/button"
