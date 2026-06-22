c8d8763b9b5d3d73335b005780b1985fdb23efa1
sarthak <sarthak.agrawal.co@gmail.com>
2026-06-22 13:51:38 +0530

    created: chat header with mobile menu toggle button

--- discord/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ discord/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,10 +1,50 @@
+import ChatHeader from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { RedirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const ChannelIdPage = () => {
-  return (
-    <div>
-      Channel iD Page!
-    </div>
-  )
+interface ChannelIdPageProps {
+  params: Promise<{
+    serverId: string;
+    channelId: string;
+  }>;
 }
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+  const { serverId, channelId } = await params;
+
+  if (!profile) {
+    return <RedirectToSignIn />;
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if (!channel || !member) {
+    redirect("/");
+  }
+
+  return (
+  <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+    <ChatHeader 
+      name={channel.name}
+      serverId={channel.serverId}
+      type="channel"
+    />
+    </div>
+);
+};
 
 export default ChannelIdPage;
