b449fa19583ec1b63d87430faf20584201e6a9e9
sarthak <sarthak.agrawal.co@gmail.com>
2026-06-27 16:56:36 +0530

    created: emoji bar for the text

--- discord/components/chat/chat-input.tsx
+++ discord/components/chat/chat-input.tsx
@@ -12,8 +12,10 @@ import {
     FormItem,
 } from "@/components/ui/form"
 import { Input } from "../ui/input";
-import { Plus, Smile } from "lucide-react";
+import { Plus } from "lucide-react";
 import {useModal} from "@/hooks/use-modal-store";
+import { EmojiPicker } from "../emoji-picker";
+import { useRouter } from "next/navigation";
 
 
 
@@ -30,6 +32,8 @@ const formSchema = z.object({
 
 export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
     const {onOpen } = useModal();
+    const router = useRouter();
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
 
@@ -49,6 +53,7 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
 
         await axios.post(url, values)
         form.reset();
+        router.refresh();
     }catch (error){
         console.log(error);
     }
@@ -80,7 +85,9 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                                     {...field}
                                 />
                                 <div className="absolute top-7 right-8 ">
-                                    <Smile/>
+                                    <EmojiPicker 
+                                        onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)}
+                                    />
                                 </div>
                             </div>
                         </FormControl>
